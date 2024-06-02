a014dbd089b3517d14a8adc394605106d8100f5f
HS <hasang2691@naver.com>
2024-06-02 22:26:57 +0900

    Feat: Apply pretendard font

--- app/src/main/java/com/example/sweatnote/screens/Writing.kt
+++ app/src/main/java/com/example/sweatnote/screens/Writing.kt
@@ -2,21 +2,16 @@ package com.example.sweatnote.screens
 
 import DiaryTextField
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,8 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.sweatnote.components.writing.CheckboxWithText
@@ -52,15 +45,12 @@ fun Writing(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("수행한 운동을 선택하세요")
-        Row(
+        LazyRow(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
+                .padding(vertical = 8.dp)
         ) {
-            for (i in 0..4) {
-                val workout = workoutOptions[i]
+            itemsIndexed(workoutOptions) { index, workout ->
                 CheckboxWithText(
                     isChecked = workout in selectedWorkouts,
                     onCheckedChange = { isChecked ->
@@ -72,21 +62,20 @@ fun Writing(navController: NavHostController) {
                     },
                     text = workout
                 )
-                Spacer(modifier = Modifier.width(8.dp))
+                if (index < workoutOptions.size - 1) {
+                    Spacer(modifier = Modifier.width(16.dp)) // 체크박스 사이에 간격을 추가합니다.
+                }
             }
         }
 
-        Text("감정을 선택하세요")
         // 감정 체크박스
-        Row(
+        Text("감정을 선택하세요")
+        LazyRow(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
+                .padding(vertical = 8.dp)
         ) {
-            for (i in 0..4) {
-                val feeling = feelings[i]
+            itemsIndexed(feelings) { index, feeling ->
                 CheckboxWithText(
                     isChecked = feeling == selectedFeeling,
                     onCheckedChange = { isChecked ->
@@ -96,9 +85,9 @@ fun Writing(navController: NavHostController) {
                     },
                     text = feeling
                 )
-                Spacer(modifier = Modifier.width(8.dp))
-
-
+                if (index < feelings.size - 1) {
+                    Spacer(modifier = Modifier.width(16.dp)) // 체크박스 사이에 간격을 추가합니다.
+                }
             }
         }
 
--- app/src/main/java/com/example/sweatnote/ui/theme/Type.kt
+++ app/src/main/java/com/example/sweatnote/ui/theme/Type.kt
@@ -2,14 +2,29 @@ package com.example.sweatnote.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.sweatnote.R
+
+val pretendardFamily = FontFamily(
+    Font(R.font.pretendard_black, FontWeight.Black),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+//        fontFamily = FontFamily.Default,
+        fontFamily = pretendardFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -32,3 +47,4 @@ val Typography = Typography(
     )
     */
 )
+
