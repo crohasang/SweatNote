debc704f61c75ccb0b9cbe67ee1cffcfeecfaaeb
glsw9639 <glsw9639@naver.com>
2024-06-12 01:22:45 +0900

    Link DB to Statistics

--- app/src/main/java/com/example/sweatnote/navigation/NavGraph.kt
+++ app/src/main/java/com/example/sweatnote/navigation/NavGraph.kt
@@ -1,31 +1,30 @@
 package com.example.sweatnote.navigation
 
-import Search
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.sweatnote.Calender.CalendarScreen
-import com.example.sweatnote.screens.*
 import com.example.sweatnote.example.DiaryViewModel
+import com.example.sweatnote.screens.*
 
 @Composable
 fun NavGraph(navController: NavHostController, diaryViewModel: DiaryViewModel) {
     NavHost(navController = navController, startDestination = Routes.Main.route) {
-        composable(Routes.Main.route) {
+        composable(route = Routes.Main.route) {
             CalendarScreen(navController)
         }
-        composable(Routes.Search.route) {
-            Search(navController)
-        }
-        composable(Routes.Statistics.route) {
-            Statistics(navController, diaryViewModel)
-        }
-        composable(Routes.Writing.route + "/{date}") { backStackEntry ->
+        composable(route = Routes.Writing.route + "/{date}") { backStackEntry ->
             val date = backStackEntry.arguments?.getString("date") ?: ""
             Writing(navController, diaryViewModel, date)
         }
-        composable(Routes.Written.route + "/{date}") { backStackEntry ->
+        composable(route = Routes.Statistics.route) {
+            Statistics(navController, diaryViewModel)
+        }
+        composable(route = Routes.Search.route) {
+            Search(navController)
+        }
+        composable(route = Routes.Written.route + "/{date}") { backStackEntry ->
             val date = backStackEntry.arguments?.getString("date") ?: ""
             Written(navController, diaryViewModel, date)
         }
--- app/src/main/java/com/example/sweatnote/navigation/Routes.kt
+++ app/src/main/java/com/example/sweatnote/navigation/Routes.kt
@@ -1,6 +1,6 @@
 package com.example.sweatnote.navigation
 
-sealed class Routes (val route: String) {
+sealed class Routes(val route: String) {
     object Main : Routes("Main")
     object Writing : Routes("Writing/{date}") {
         fun createRoute(date: String) = "Writing/$date"
