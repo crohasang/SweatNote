9c02dd4395dbe718d04f305d291052f39a93e465
HS <hasang2691@naver.com>
2024-06-15 00:57:58 +0900

    Feat: Apply animation when doesn't write(or choose) anything in Writing and Search Screen

--- app/src/main/java/com/example/sweatnote/screens/Search.kt
+++ app/src/main/java/com/example/sweatnote/screens/Search.kt
@@ -2,6 +2,9 @@ package com.example.sweatnote.screens
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,13 +12,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -34,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,24 +57,42 @@ fun Search(navController: NavHostController, viewModel: DiaryViewModel = viewMod
     var searchResults by remember { mutableStateOf<List<Diary>>(emptyList()) }
     var searchPerformed by remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
-    val scrollState = rememberScrollState()
-
+    val isTextBlank = remember { mutableStateOf(false) }
+    val shake by remember {mutableStateOf(Animatable(0f))}
+    val keyboardController = LocalSoftwareKeyboardController.current
 
 
     fun handleSearchClick() {
-        scope.launch {
-            try {
-                viewModel.searchDiariesByKeyword(text).collect { results ->
-                    searchResults = results
-                    searchPerformed = true
-                    Log.d("Search", "Search results: $searchResults")
+        // 검색 텍스트가 비어있는지 확인
+        if (text.isBlank()) {
+            isTextBlank.value = true
+
+            scope.launch {
+                shake.animateTo(
+                    targetValue = 10f,
+                    animationSpec = tween(
+                        durationMillis = 200,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+                shake.snapTo(0f)
+            }
+        } else {
+            isTextBlank.value = false
+            scope.launch {
+                try {
+                    viewModel.searchDiariesByKeyword(text).collect { results ->
+                        searchResults = results
+                        searchPerformed = true
+                        Log.d("Search", "Search results: $searchResults")
+                    }
+                } catch (e: Exception) {
+                    Log.e("Search", "Error during search", e)
                 }
-            } catch (e: Exception) {
-                Log.e("Search", "Error during search", e)
             }
         }
-
     }
+
     Scaffold(
         topBar = { TopBar(navController) },
         content = {
@@ -79,8 +100,7 @@ fun Search(navController: NavHostController, viewModel: DiaryViewModel = viewMod
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(top = 40.dp)
-                    .padding(bottom = 80.dp)
-                    .verticalScroll(scrollState),
+                    .padding(bottom = 80.dp),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -109,16 +129,21 @@ fun Search(navController: NavHostController, viewModel: DiaryViewModel = viewMod
                     shape = RoundedCornerShape(50), // 타원형 모양
                     colors = TextFieldDefaults.outlinedTextFieldColors(
                         cursorColor = Color.Black,
-                        focusedBorderColor = Color.Black,
-                        unfocusedBorderColor = Color.Black
+                        focusedBorderColor = if(isTextBlank.value) Color.Red else Color.Black,
+                        unfocusedBorderColor = if(isTextBlank.value) Color.Red else Color.Black
                     ),
                     singleLine = true,
                     modifier = Modifier
                         .fillMaxWidth(0.8f)
-                        .background(Color(0xFFF5F5F5)),
+                        .background(Color(0xFFF5F5F5))
+                        .offset(x = shake.value.dp),
                     keyboardActions = KeyboardActions(
-                        onDone = { handleSearchClick() }
+                        onDone = {
+                            handleSearchClick()
+                            keyboardController?.hide()
+                        }
                     )
+
                 )
 
                 Spacer(modifier = Modifier.height(20.dp))
